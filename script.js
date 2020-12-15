efcc40c9e6db7d27b10aa711b754a14ab2fa978c
MINAKO.P <69062635+MNK375@users.noreply.github.com>
2020-12-15 14:51:37 -0700

    changed the candidates name

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var createPolitician= function(name, partyColor){
 
   var politician= {};
@@ -20,31 +21,31 @@ var createPolitician= function(name, partyColor){
   return politician;        
 }
 
-const omer = createPolitician("omer", [245, 141, 136]);
-const miray = createPolitician("miray", [132, 17, 11]);
+const Trump = createPolitician("Trump", [245, 141, 136]);
+const Biden = createPolitician("Biden", [132, 17, 11]);
 
 
-omer.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
-miray.electionResults  = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+Trump.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+Biden.electionResults  = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 
-omer.tallyUpTotalVotes();
-miray.tallyUpTotalVotes();
+Trump.tallyUpTotalVotes();
+Biden.tallyUpTotalVotes();
 
-omer.electionResults[9]=1;
-miray.electionResults[9]=28;
+Trump.electionResults[9]=1;
+Biden.electionResults[9]=28;
 
-omer.electionResults[4]=17;
-miray.electionResults[4]=38;
+Trump.electionResults[4]=17;
+Biden.electionResults[4]=38;
 
-omer.electionResults[43]=11;
-miray.electionResults[43]=27;
+Trump.electionResults[43]=11;
+Biden.electionResults[43]=27;
 
 var winner = function ()
 { 
-if (omer.totalVotes > miray.totalVotes){
-    winner = omer.name;
-}else if (omer.totalVotes < miray.totalVotes){
-    winner = miray.name;
+if (Trump.totalVotes > Biden.totalVotes){
+    winner = Trump.name;
+}else if (Trump.totalVotes < Biden.totalVotes){
+    winner = Biden.name;
 }else{
     winner = "DRAW"
 }
@@ -53,12 +54,12 @@ winner ();
    
 var setStateResults = function(state){
   theStates[state].winner = null;
-  if (omer.electionResults [state]  > miray.electionResults[state])
+  if (Trump.electionResults [state]  > Biden.electionResults[state])
       {
-    theStates[state].winner = omer;
+    theStates[state].winner = Trump;
   } else if
-   (omer.electionResults [state] < miray.electionResults[state]){
-    theStates[state].winner = miray;
+   (Trump.electionResults [state] < Biden.electionResults[state]){
+    theStates[state].winner = Biden;
   }
   
 
@@ -82,10 +83,10 @@ var setStateResults = function(state){
 
   stateName.innerText = theStates[state].nameFull;
   abbrev.innerText = "(" +theStates[state].nameAbbrev + ")";
-  candidate1Name.innerText = omer.name;
-  candidate2Name.innerText = miray.name;
-  candidate1Results.innerText = omer.electionResults[state];
-  candidate2Results.innerText = miray.electionResults[state];
+  candidate1Name.innerText = Trump.name;
+  candidate2Name.innerText = Biden.name;
+  candidate1Results.innerText = Trump.electionResults[state];
+  candidate2Results.innerText = Biden.electionResults[state];
   if (theStates[state].winner === null){
     winnersName.innerText = "DRAW";
 } else {
@@ -97,9 +98,9 @@ var setStateResults = function(state){
   var countryInfoTable = document.getElementById('countryResults');
   var row = countryInfoTable.children[0].children[0];
   
-   row.children[0].innerText = omer.name;
-   row.children[1].innerText = omer.totalVotes;
-  row.children[2].innerText = miray.name;
-  row.children[3].innerText = miray.totalVotes;
+   row.children[0].innerText = Trump.name;
+   row.children[1].innerText = Trump.totalVotes;
+  row.children[2].innerText = Biden.name;
+  row.children[3].innerText = Biden.totalVotes;
   row.children[5].innerText = winner;
 }
